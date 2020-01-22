e35002d1783c9f589421a0fc1195c17f1a4ea485
Simon Mölzer <evilhowser@gmail.com>
2020-01-22 16:12:52 +0100

    Add root mean square error to localization comparison

--- comparison/localizationcompare.py
+++ comparison/localizationcompare.py
@@ -10,6 +10,27 @@ gmapping_2 = np.asarray([[0, 0],
             [3.10, 0.52],
             [0.03, 0.35]])
 
+corner_2 = np.asarray([[0, 0],
+            [-0.20, 2.33],
+            [1.14, 3.53],
+            [3.08, 3.74],
+            [2.31, 1.79],
+            [-0.43, 2.68]])
+
+split_merge_2 = np.asarray([[0, 0],
+            [-0.11, 2.43],
+            [1.69, 3.80],
+            [2.58, 4.42],
+            [3.13, 0.53],
+            [0.07, 0.25]])
+
+line_segment_2 = np.asarray([[0, 0],
+            [-0.11, 2.40],
+            [1.86, 3.82],
+            [3.26, 4.03],
+            [3.11, 0.50],
+            [0.03, 0.49]])
+
 ground_truth_2 = np.asarray([[0.51, 0.49],
             [0.63, 2.93],
             [2.63, 4.10],
@@ -24,6 +45,10 @@ def get_score(data, ground_truth):
     offset_dataset = offset_data(data, ground_truth[0][0], ground_truth[0][1])
 
     res = np.subtract(ground_truth, offset_dataset)
-    return np.sqrt(np.sum(res ** 2, axis=1)).sum()
+    res = np.sqrt(np.sum(res ** 2, axis=1))
+    return np.sqrt(np.mean(res ** 2))
 
 print(get_score(gmapping_2, ground_truth_2))
+print(get_score(corner_2, ground_truth_2))
+print(get_score(split_merge_2, ground_truth_2))
+print(get_score(line_segment_2, ground_truth_2))
