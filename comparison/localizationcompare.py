575f9726e29d7c6e315b3dfcfbce327023c0380d
Simon Mölzer <evilhowser@gmail.com>
2020-01-22 16:01:13 +0100

    Add localization comparison script

---
+++ comparison/localizationcompare.py
@@ -0,0 +1,29 @@
+import numpy as np
+# map 1 RAW
+
+
+# map 2 RAW
+gmapping_2 = np.asarray([[0, 0],
+            [-0.07, 2.41],
+            [1.76, 3.78],
+            [2.74, 4.05],
+            [3.10, 0.52],
+            [0.03, 0.35]])
+
+ground_truth_2 = np.asarray([[0.51, 0.49],
+            [0.63, 2.93],
+            [2.63, 4.10],
+            [3.60, 4.08],
+            [3.58, 0.69],
+            [0.60, 0.74]])
+
+def offset_data(data, x_offset, y_offset):
+    return np.add(data, np.asarray([x_offset, y_offset]))
+
+def get_score(data, ground_truth):
+    offset_dataset = offset_data(data, ground_truth[0][0], ground_truth[0][1])
+
+    res = np.subtract(ground_truth, offset_dataset)
+    return np.sqrt(np.sum(res ** 2, axis=1)).sum()
+
+print(get_score(gmapping_2, ground_truth_2))
